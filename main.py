f505fe8ef985ab38085e63bab42402ab4068442b
shm <s.mudda@dautomation.com>
2023-07-21 10:58:02 +0200

    added pandas and extracted from dynamic data

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    temperature = 23
+    file_name = 'data_small/TG_STAID' + str(station).zfill(6) +'.txt'
+    df = pd.read_csv(file_name, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
 
     return {'station': station,
             'data': date,
