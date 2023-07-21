29c0b792ebcd2b5a1cd1fe0ec1d62a46320d432b
shm <s.mudda@dautomation.com>
2023-07-21 10:21:38 +0200

    modified some changes

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask('website')
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = 23
+
+    return {'station': station,
+            'data': date,
+            'temperature': temperature}
 
 app.run(debug=True)
